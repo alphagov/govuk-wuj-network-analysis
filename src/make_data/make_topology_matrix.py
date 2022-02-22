1f3bf18ae3c1a04f9d95089c9516255f3584f65e
Alice Stephenson <alice.stephenson@digital.cabinet-office.gov.uk>
2022-02-22 14:37:34 +0000

    Updated topology matrix code

--- src/make_data/make_topology_matrix.py
+++ src/make_data/make_topology_matrix.py
@@ -1,5 +1,6 @@
 """
-FROM: https://github.com/alphagov/govuk-intent-detector/blob/main/src/make_data/make_topology_matrix.py
+FROM:
+https://github.com/alphagov/govuk-intent-detector/blob/main/src/make_data/make_topology_matrix.py
 """
 
 import re
@@ -26,14 +27,19 @@ def clean_url(url: str) -> Union[str, None]:
     parsed = urlparse(url)
 
     # remove schema and domain from https://gov.uk urls that still have it specified
-    if parsed.netloc == 'www.gov.uk':
-        if parsed.path == '':
-            return '/'
+    if parsed.netloc == "www.gov.uk":
+        if parsed.path == "":
+            return "/"
         else:
-            return parsed.path if parsed.fragment == '' else parsed.path + '#' + parsed.fragment
+            return (
+                parsed.path
+                if parsed.fragment == ""
+                else parsed.path + "#" + parsed.fragment
+            )
 
-    # cross-domain and external urls, mailto addresses, which have scheme (e.g., https://) specified
-    if parsed.scheme != '':
+    # cross-domain and external urls, mailto addresses, which have scheme
+    # (e.g., https://) specified
+    if parsed.scheme != "":
         return None
     else:
         return url
@@ -42,12 +48,16 @@ def clean_url(url: str) -> Union[str, None]:
 def combine_anchor2url(possible_anchor: str, main_url: str) -> str:
     """Attached anchor to the main_url"""
 
-    return possible_anchor if (not possible_anchor) or (
-        not possible_anchor.startswith("#")) else main_url + possible_anchor
+    return (
+        possible_anchor
+        if (not possible_anchor) or (not possible_anchor.startswith("#"))
+        else main_url + possible_anchor
+    )
 
 
 def process_page_links(
-        page_links: Dict[str, List[Optional[str]]]) -> Dict[str, List[str]]:
+    page_links: Dict[str, List[Optional[str]]]
+) -> Dict[str, List[str]]:
     """
     Process the hyperlinks in a page by:
     - removing duplicates
@@ -55,50 +65,41 @@ def process_page_links(
     - removing schema and main domain ("www.gov.uk") from gov.uk urls
     - removing cross-domain or external urls.
     Args:
-        page_links: A dictionary where the key is a page url, and the value is a List of the page's
-                    embedded hyperlinks (as string).
+        page_links: A dictionary where the key is a page url, and the value is a List of
+                    the page's embedded hyperlinks (as string).
     Returns:
-        A dictionary of where the key is the page url from ``page_links``, and the value is the List of
-        the page's embedded hyperlinks that have been processed as above.
+        A dictionary of where the key is the page url from ``page_links``, and the value
+        is the list of the page's embedded hyperlinks that have been processed as above.
     """
     return {
         page: list(
             set(
-                filter(None, [
-                    combine_anchor2url(clean_url(link), page) for link in links
-                ])))
+                filter(
+                    None, [combine_anchor2url(clean_url(link), page) for link in links]
+                )
+            )
+        )
         for page, links in page_links.items()
     }
 
 
-def create_topology_matrix_pd(
-        page_links: Dict[str, List[str]]) -> pd.DataFrame:
+def create_topology_matrix_pd(page_links: Dict[str, List[str]]) -> pd.DataFrame:
     """
-    Generate the adjacency topology matrix from a dictionary where the key is the 'source url' and
-    the value is a list of the url's embedded links (a.k.a. destination urls) that are also 'source urls'.
+    Generate the adjacency topology matrix from a dictionary where the key is the
+    'source url' and the value is a list of the url's embedded links (a.k.a.
+    destination urls).
     Args:
-        page_links: A dictionary where the key is a page url, and the value is a List of the page's
-                    embedded hyperlinks (a.k.a. destination urls)
+        page_links: A dictionary where the key is a page url, and the value is a List of
+                    the page's embedded hyperlinks (a.k.a. destination urls)
     Returns:
         A pandas.DataFrame representing the directed adjacency topology matrix.
     """
 
-    edges = [(page, link) for page, links in page_links.items()
-             for link in links if link in page_links.keys()]
+    edges = [(page, link) for page, links in page_links.items() for link in links]
 
     df = pd.DataFrame(edges)
 
     adjacency_matrix = pd.crosstab(df[0], df[1], dropna=False)
-
-    # add columns for unlinked source urls
-    columns_to_add = [
-        ind for ind in adjacency_matrix.index.values
-        if ind not in adjacency_matrix.columns
-    ]
-    adjacency_matrix = adjacency_matrix.assign(
-        **dict.fromkeys(columns_to_add, 0))
-    order_of_columns = adjacency_matrix.index.values
-    adjacency_matrix = adjacency_matrix[order_of_columns]
 
     adjacency_matrix.columns.name = None
     adjacency_matrix.index.name = None
@@ -122,23 +123,16 @@ def ingest_html(file_info: Dict[str, Path]) -> Dict[str, List[str]]:
     for page_url, filepath in file_info.items():
         with open(filepath, mode="r", encoding="utf-8") as f:
             links = BeautifulSoup(
-                f.read(),
-                parse_only=SoupStrainer('a'),
-                features="lxml"
+                f.read(), parse_only=SoupStrainer("a"), features="lxml"
             )
             hrefs = [
-                link.get('href')
-                for link in links
-                if not isinstance(link, Doctype)
+                link.get("href") for link in links if not isinstance(link, Doctype)
             ]
             page_hyperlinks_dict[page_url] = [
                 url
                 for url in hrefs
-                if (
-                    url is not None
-                ) and (
-                    not url.startswith("#")  # page links to itself
-                )
+                if (url is not None)
+                and (not url.startswith("#"))  # page links to itself
             ]
 
     return page_hyperlinks_dict
@@ -169,7 +163,7 @@ def process_links(links: Dict[str, List[str]]) -> Dict[str, List[str]]:
     for page_url, links_list in links.items():
 
         # strip fragments and/or parameters
-        links_list_proc = [re.split('[#?]', link)[0] for link in links_list]
+        links_list_proc = [re.split("[#?]", link)[0] for link in links_list]
 
         # remove external and cross-domain links, clean schema from gov.uk urls
         # (also remove None results)
@@ -178,10 +172,16 @@ def process_links(links: Dict[str, List[str]]) -> Dict[str, List[str]]:
         )
 
         # remove special edge cases and omit duplicates
-        links_list_proc = list(set([
-            link for link in links_list_proc if PurePosixPath(link).stem not in
-            ['preview', 'y', 'results', 'questions']
-        ]))
+        links_list_proc = list(
+            set(
+                [
+                    link
+                    for link in links_list_proc
+                    if PurePosixPath(link).stem
+                    not in ["preview", "y", "results", "questions"]
+                ]
+            )
+        )
 
         # enter the modified URLs into a dictionary.  Even if there are no
         # links, entering the key means that the page will be represented in
@@ -189,4 +189,3 @@ def process_links(links: Dict[str, List[str]]) -> Dict[str, List[str]]:
         results[page_url] = links_list_proc
 
     return results
-    
